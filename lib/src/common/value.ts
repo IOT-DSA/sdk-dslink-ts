626a2ce1ad2e7e1e62d1b942e2f72de2a041f8c2
Rick Zhou <rinick@gmail.com>
2019-01-07 14:55:37 -0800

    fix value.ts

--- lib/src/common/value.ts
+++ lib/src/common/value.ts
@@ -1,51 +1,53 @@
-// part of dslink.common;
+type ValueUpdateCallback = (update: ValueUpdate) => any;
 
-typedef T ValueUpdateCallback<T>(update: ValueUpdate);
-typedef T ValueCallback<T>(value);
+type ValueCallback<T> = (value: T) => any;
+
+const TIME_ZONE = (function () {
+  let timeZoneOffset: number = (new Date()).getTimezoneOffset();
+  let s = "+";
+  if (timeZoneOffset < 0) {
+    timeZoneOffset = -timeZoneOffset;
+    s = "-";
+  }
+  let hhstr = `${(timeZoneOffset / 60) | 0}`.padStart(2, '0');
+  let mmstr = `${timeZoneOffset % 60}`.padStart(2, '0');
+  return `${s}${hhstr}:${mmstr}`;
+})();
+
 
 /// Represents an update to a value subscription.
-export class ValueUpdate  {
+export class ValueUpdate {
   /// DSA formatted timezone.
-  static readonly string TIME_ZONE = () {
-    timeZoneOffset:number = (new DateTime.now()).timeZoneOffset.inMinutes;
-    string s = "+";
-    if (timeZoneOffset < 0) {
-      timeZoneOffset = -timeZoneOffset;
-      s = "-";
-    }
-    hh:number = timeZoneOffset ~/ 60;
-    mm:number = timeZoneOffset % 60;
-    return "$s${hh < 10 ? '0' : ''}$hh:${mm < 10 ? "0" : ''}$mm";
-  }();
-
   static _lastTsStr: string;
-  static _lastTs:number = 0;
+  static _lastTs: number = 0;
+
   /// Generates a timestamp in the proper DSA format.
-  static getTs():string {
-    DateTime d = new DateTime.now();
-    if (d.millisecondsSinceEpoch == this._lastTs) {
+  static getTs(): string {
+    let d = new Date();
+    if (d.getTime() == this._lastTs) {
       return this._lastTsStr;
     }
-    _lastTs = d.millisecondsSinceEpoch;
-    _lastTsStr = "${d.toIso8601String()}$TIME_ZONE";
+    ValueUpdate._lastTs = d.getTime();
+    let offsetISOStr = new Date(d.getTime() - (d.getTimezoneOffset() * 60000)).toISOString();
+    ValueUpdate._lastTsStr = `${offsetISOStr.slice(0, -1)}${TIME_ZONE}`;
     return this._lastTsStr;
   }
 
   /// The id of the ack we are waiting for.
-  waitingAck:number = -1;
+  waitingAck: number = -1;
 
   /// The value for this update.
-  value: object;
+  value: any;
 
   /// A [string] representation of the timestamp for this value.
   ts: string;
 
-  _timestamp: DateTime;
+  _timestamp: Date;
 
   /// Gets a [DateTime] representation of the timestamp for this value.
-  get timestamp(): DateTime {
-    if ( this._timestamp == null) {
-      _timestamp = DateTime.parse(ts);
+  get timestamp(): Date {
+    if (this._timestamp == null) {
+      this._timestamp = new Date(this.ts);
     }
     return this._timestamp;
   }
@@ -54,163 +56,89 @@ export class ValueUpdate  {
   status: string;
 
   /// How many updates have happened since the last response.
-  count:number;
+  count: number = 1;
 
-  /// The sum value if one or more numeric values has been skipped.
-  sum: num;
-
-  /// The minimum value if one or more numeric values has been skipped.
-  min: num;
-
-  /// The maximum value if one or more numeric values has been skipped.
-  max: num;
 
   /// The timestamp for when this value update was created.
-  created: DateTime;
+  created: Date;
 
-  ValueUpdate(this.value,
-      {this.ts,
-      let meta: object,
-      this.status,
-      this.count: 1,
-      this.sum: double.NAN,
-      this.min: double.NAN,
-      this.max: double.NAN}) {
-    if (ts == null) {
-      ts = getTs();
-    }
-
-    created = new DateTime.now();
-
-    if (meta != null) {
-      if (meta["count"] is int) {
-        count = meta["count"];
-      } else if (value == null) {
-        count = 0;
+  constructor(value: any, options?:
+    {
+      ts?: string,
+      status?: string,
+      count?: number
+    }) {
+    if (options) {
+      if (options.ts) {
+        this.ts = options.ts;
       }
-
-      if (typeof meta["status"] === 'string') {
-        status = meta["status"];
+      if (options.status) {
+        this.status = options.status;
       }
-
-      if (meta["sum"] is num) {
-        sum = meta["sum"];
-      }
-
-      if (meta["max"] is num) {
-        max = meta["max"];
-      }
-
-      if (meta["min"] is num) {
-        min = meta["min"];
+      if (options.count) {
+        this.count = options.count;
       }
     }
-
-    if ( typeof value === 'number' && count == 1) {
-      if (sum != sum) sum = value;
-      if (max != max) max = value;
-      if (min != min) min = value;
+    if (!this.ts) {
+      this.ts = ValueUpdate.getTs();
     }
+
+    this.created = new Date();
+
   }
 
-  ValueUpdate.merge(oldUpdate: ValueUpdate, newUpdate: ValueUpdate) {
-    value = newUpdate.value;
-    ts = newUpdate.ts;
-    status = newUpdate.status;
-    count = oldUpdate.count + newUpdate.count;
-    sum = oldUpdate.sum;
-    if (!newUpdate.sum.isNaN) {
-      if (sum == sum) {
-        sum = newUpdate.sum;
-      } else {
-        sum += newUpdate.sum;
-      }
-    }
-    min = oldUpdate.min;
-    if (min.isNaN || newUpdate.min < min) {
-      min = newUpdate.min;
-    }
-    max = oldUpdate.min;
-    if (max.isNaN || newUpdate.max > max) {
-      max = newUpdate.max;
-    }
-
-    created = newUpdate.created;
+  static merge(oldUpdate: ValueUpdate, newUpdate: ValueUpdate) {
+    let newValue = new ValueUpdate(null);
+    newValue.value = newUpdate.value;
+    newValue.ts = newUpdate.ts;
+    newValue.status = newUpdate.status;
+    newValue.count = oldUpdate.count + newUpdate.count;
+    newValue.created = newUpdate.created;
+    return newValue;
   }
 
-  _latency: Duration;
+  _latency: number;
 
   /// Calculates the latency
-  get latency(): Duration {
-    if ( this._latency == null) {
-      _latency = created.difference(timestamp);
+  get latency() {
+    if (!this._latency) {
+      this._latency = this.timestamp.getTime() - this.created.getTime();
     }
     return this._latency;
   }
 
   /// merge the new update into existing instance
   mergeAdd(newUpdate: ValueUpdate) {
-    value = newUpdate.value;
-    ts = newUpdate.ts;
-    status = newUpdate.status;
-    count += newUpdate.count;
+    this.value = newUpdate.value;
+    this.ts = newUpdate.ts;
+    this.status = newUpdate.status;
+    this.count += newUpdate.count;
 
-    if (!newUpdate.sum.isNaN) {
-      if (sum == sum) {
-        sum += newUpdate.sum;
-      } else {
-        sum = newUpdate.sum;
-      }
-    }
-    if (min != min || newUpdate.min < min) {
-      min = newUpdate.min;
-    }
-    if (max != max || newUpdate.max > max) {
-      max = newUpdate.max;
-    }
+
   }
 
-  equals(other: ValueUpdate):boolean {
-    if ( (value != null && value instanceof Object) ) {
-      // assume object is same if it's generated at same timestamp
-      if (other.value is! object) {
-        return false;
-      }
-    } else if ( Array.isArray(value) ) {
+  equals(other: ValueUpdate): boolean {
+    if (Array.isArray(this.value)) {
       // assume List is same if it's generated at same timestamp
-      if (other.value is! List) {
+      if (!Array.isArray(other.value)) {
         return false;
       }
-    } else if (value != other.value) {
+    } else if ((this.value != null && this.value instanceof Object)) {
+      // assume object is same if it's generated at same timestamp
+      if (!(other.value instanceof Object)) {
+        return false;
+      }
+    } else if (!Object.is(this.value, other.value)) {
       return false;
     }
-
-    if (other.ts != ts || other.count != count) {
-      return false;
-    }
-
-    if (count == 1) {
-      return true;
-    }
-    return other.sum == sum && other.min == min && other.max == max;
+    return other.ts === this.ts && other.count === this.count;
   }
 
   /// Generates a map representation of this value update.
-  toMap():object {
-    object m = {"ts": ts, "value": value};
-    if (count == 0) {
-      m["count"] = 0;
-    } else if (count > 1) {
-      m["count"] = count;
-      if (sum.isFinite) {
-        m["sum"] = sum;
-      }
-      if (max.isFinite) {
-        m["max"] = max;
-      }
-      if (min.isFinite) {
-        m["min"] = min;
-      }
+  toMap(): { [key: string]: any } {
+    let m: any = {"ts": this.ts, "value": this.value};
+    if (this.count != 1) {
+      m["count"] = this.count;
     }
     return m;
   }
@@ -219,20 +147,20 @@ export class ValueUpdate  {
   storedData: object;
 
   _cloned: boolean = false;
-  ValueUpdate cloneForAckQueue(){
-    if (!_cloned) {
-      _cloned = true;
+
+  cloneForAckQueue(): ValueUpdate {
+    if (!this._cloned) {
+      this._cloned = true;
       return this;
     }
 
     return new ValueUpdate(
-      value,
-      ts: ts,
-      status: status,
-      count: count,
-      sum: sum,
-      min: min,
-      max: max
+      this.value,
+      {
+        ts: this.ts,
+        status: this.status,
+        count: this.count
+      }
     );
   }
 }
