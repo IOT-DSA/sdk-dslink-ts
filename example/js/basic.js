2afcf22a942de3f68c8e36ffc4dfd70830392ea3
rinick <rinick@gmail.com>
2019-02-11 16:34:26 -0800

    update the doc to use the Promise version of base api

--- example/js/basic.js
+++ example/js/basic.js
@@ -1,7 +1,6 @@
 // const {DSLink} = require('dslink-js/js/web');
 const {DSLink} = require('../../js/web');
 
-
 async function main() {
     let link = new DSLink('ws://localhost:8080/ws', 'json');
     link.connect();
