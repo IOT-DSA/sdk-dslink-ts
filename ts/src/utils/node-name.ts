dc1f4a22b4a5aa621736adf8a3ed06767128e56f
rinick <rinick@gmail.com>
2021-12-16 12:48:08 -0800

    test decodeEnums

--- ts/src/utils/node-name.ts
+++ ts/src/utils/node-name.ts
@@ -7,8 +7,21 @@ function escapeNodeName(match: string) {
 }
 
 export function encodeNodeName(name: string) {
-  return name.replace(/[\u0000-\u001f/\\?*:|"<>%]/g, escapeNodeName);
+  return name.replace(/[\u0000-\u001f/\\?*:|"<>%,\u007f]/g, escapeNodeName);
 }
+
 export function decodeNodeName(name: string) {
   return decodeURIComponent(name);
+}
+
+/**
+ * decode dsa enum string in the format of [optionA,optionB,optionC]
+ * @param enums
+ */
+export function decodeEnums(enums: string) {
+  let targetString = enums;
+  if (targetString.startsWith('[') && targetString.endsWith(']')) {
+    targetString = targetString.substring(1, targetString.length - 1);
+  }
+  return targetString.split(',').map((s) => decodeNodeName(s));
 }
